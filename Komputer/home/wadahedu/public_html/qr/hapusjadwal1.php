e4eac7c64a57b89130c4a05ff5154e8bd2c3f002
Nizar <40895148+nizariyf@users.noreply.github.com>
2021-03-28 15:03:40 +0700

    Delete Komputer directory

--- Komputer/home/wadahedu/public_html/qr/hapusjadwal1.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include "koneksi.php";
- $id = $_POST['id_anggota'];
- 
- mysqli_query($kon, "delete from jadwal where id='$id'")
-										or die (mysql_error() );
-			echo "<script>;window.location='home.php?state=jadwal'</script>";								
-?>
--- Komputer/home/wadahedu/public_html/qr/hapuskelas1.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include "koneksi.php";
- $id = $_POST['id_anggota'];
- 
- mysqli_query($kon, "delete from kelas where kelas='$id'")
-										or die (mysql_error() );
-			echo "<script>;window.location='home.php?state=daftarkelas'</script>";								
-?>
