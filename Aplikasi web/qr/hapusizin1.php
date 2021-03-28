9b1ab2ec55670cdf2933715f96cdaae1ff1c29fe
Nizar <40895148+nizariyf@users.noreply.github.com>
2021-03-28 15:04:40 +0700

    Delete Aplikasi web directory

--- Aplikasi web/qr/hapusizin1.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include "koneksi.php";
- $id = $_POST['id_anggota'];
- 
- mysqli_query($kon, "delete from izin where urutan='$id'")
-										or die (mysql_error() );
-			echo "<script>;window.location='home.php?state=rekapizin'</script>";								
-?>
--- Aplikasi web/qr/tambahjadwal.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "koneksi.php";
-mysqli_query($kon, "INSERT INTO jadwal(jenis,jam_masuk,jam_pulang) VALUES ('$_POST[jenis]','$_POST[masuk]' , '$_POST[pulang]' )")or die ("Query Gagal ".mysql_error());
-
-echo "<script>;window.location='home.php?state=jadwal'</script>";
-?>
